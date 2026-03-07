8e431004328be7c28544930ff1b6912193b75b66
javad.asadi <javad.asadi@flix.com>
2026-03-07 13:02:42 +0100

    Add DartExtendedLanguageServer, NoOpDartLanguageClient.kt

---
+++ third_party/src/main/java/com/jetbrains/lang/dart/analyzer/lsp/DartExtendedLanguageServer.kt
@@ -0,0 +1,13 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.jetbrains.lang.dart.analyzer.lsp
+
+import org.eclipse.lsp4j.jsonrpc.services.JsonRequest
+import org.eclipse.lsp4j.services.LanguageServer
+import java.util.concurrent.CompletableFuture
+
+internal interface DartExtendedLanguageServer : LanguageServer {
+  @JsonRequest("dart/diagnosticServer")
+  fun diagnosticServer(): CompletableFuture<DartDiagnosticServerResult>
+}
+
+internal data class DartDiagnosticServerResult(val port: Int = 0)
---
+++ third_party/src/main/java/com/jetbrains/lang/dart/analyzer/lsp/NoOpDartLanguageClient.kt
@@ -0,0 +1,23 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.jetbrains.lang.dart.analyzer.lsp
+
+import org.eclipse.lsp4j.MessageActionItem
+import org.eclipse.lsp4j.MessageParams
+import org.eclipse.lsp4j.PublishDiagnosticsParams
+import org.eclipse.lsp4j.ShowMessageRequestParams
+import org.eclipse.lsp4j.services.LanguageClient
+import java.util.concurrent.CompletableFuture
+
+internal class NoOpDartLanguageClient : LanguageClient {
+  override fun telemetryEvent(`object`: Any?) {}
+
+  override fun publishDiagnostics(diagnostics: PublishDiagnosticsParams) {}
+
+  override fun showMessage(messageParams: MessageParams) {}
+
+  override fun showMessageRequest(requestParams: ShowMessageRequestParams): CompletableFuture<MessageActionItem> {
+    return CompletableFuture.completedFuture(null)
+  }
+
+  override fun logMessage(message: MessageParams) {}
+}
